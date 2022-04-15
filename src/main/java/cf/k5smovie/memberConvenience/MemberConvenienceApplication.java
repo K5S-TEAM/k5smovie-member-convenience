3fd0ef202073fc4ee109b2d312ab99031c6aff98
Hyun Ku Kim <presentnine@naver.com>
2022-04-15 12:08:53 +0900

    init

---
+++ src/main/java/cf/k5smovie/memberConvenience/MemberConvenienceApplication.java
@@ -0,0 +1,13 @@
+package cf.k5smovie.memberConvenience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberConvenienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberConvenienceApplication.class, args);
+	}
+
+}
